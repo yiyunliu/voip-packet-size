3a050d2c7147781fb7ca3f12d995251b13d9bc07
Yiyun Liu <liuyiyun@terpmail.umd.edu>
2021-04-08 13:45:44 -0400

    save

--- analysis.R
+++ analysis.R
@@ -1,12 +1,14 @@
 # https://dplyr.tidyverse.org/reference/join.html
 # Right join as zip
 library(tidyverse)
-## library(class)
+library(class)
 library(C50)
 ## library(neuralnet)
 
 
 dt_en <- read_csv("english.csv", col_names = c("socket_number", "packet_size"), col_types = cols(socket_number = col_integer(), packet_size = col_integer()), comment = "#") %>% filter(socket_number == 77)
+
+dt_biden <- read_csv("biden_speech.csv", col_names = c("socket_number", "packet_size"), col_types = cols(socket_number = col_integer(), packet_size = col_integer()), comment = "#") %>% filter(socket_number == 53)
 
 dt_cn <- read_csv("chinese.csv", col_names = c("socket_number", "packet_size"), col_types = cols(socket_number = col_integer(), packet_size = col_integer()), comment = "#") %>% filter(socket_number == 76)
 
@@ -16,6 +18,10 @@ dt_cn_contrib <- read_csv("cn_contrib.csv", col_names = c("socket_number", "pack
 dt_jp <- read_csv("japanese.csv", col_names = c("socket_number", "packet_size"), col_types = cols(socket_number = col_integer(), packet_size = col_integer()), comment = "#") %>% filter(socket_number==79)
 
 dt_gsl <- read_csv("gsl_eng.csv", col_names = c("socket_number", "packet_size"), col_types = cols(socket_number = col_integer(), packet_size = col_integer()), comment = "#") %>% filter(socket_number==78)
+
+dt_gsl_cn <- read_csv("gsl_cn.csv", col_names = c("socket_number", "packet_size"), col_types = cols(socket_number = col_integer(), packet_size = col_integer()), comment = "#") %>% filter(socket_number==78)
+
+
 
 ## build_3_gram(dt_eng) %>% mutate(language='english')
 
@@ -33,7 +39,7 @@ build_3_gram <- function(dt) {
 label_and_split <- function(dt, label) {
     dt <- dt %>% mutate(language = factor(label, levels=languages))
     second_half <- dt %>% slice((n() %/% 2 + 1) : n())
-    first_half <- dt
+    first_half <- dt ## %>% slice(1 : (n() %/% 2))
     ## list(first_half %>% pull(language), second_half %>% select(-language)))
     list("first" = list("data" = first_half %>% select(-language), "class" = first_half %>% pull(language)), "second" = list("data" = second_half %>% select(-language), "class" = second_half %>% pull(language)))
 }
@@ -51,25 +57,47 @@ cn_test <- cn_train_and_test$second
 jp_train <- jp_train_and_test$first
 jp_test <- jp_train_and_test$second
 
-## knn(bind_rows(gsl_train$data,cn_train$data,jp_train$data), bind_rows(gsl_test$data,cn_test$data,jp_test$data), c(gsl_train$class,cn_train$class,jp_train$class), k = 50)
+knn(bind_rows(en_train$data,cn_train$data,jp_train$data), bind_rows(en_test$data,cn_test$data,jp_test$data), c(en_train$class,cn_train$class,jp_train$class))
 
 
 gsl_test <- dt_gsl %>% build_3_gram() %>% mutate(language= factor('English', levels=languages))
 
-m <- C5.0(bind_rows(en_train$data,cn_train$data,jp_train$data, gsl_test %>% select(-language)), fct_c(en_train$class,cn_train$class,jp_train$class, gsl_test %>% pull(language)))
+gsl_cn_test <- dt_gsl_cn %>% build_3_gram() %>% mutate(language= factor('Chinese', levels=languages))
 
-## cn_p <- predict(m, cn_test$data, type="class")
-## print(sum(cn_p == "Chinese")/length(cn_p)) 
+m <- C5.0(bind_rows(en_train$data,cn_train$data,jp_train$data, gsl_test %>% select(-language), gsl_cn_test %>% select(-language)), fct_c(en_train$class,cn_train$class,jp_train$class, gsl_test %>% pull(language), gsl_cn_test %>% pull(language)))
 
-## en_p <- predict(m, en_test$data, type="class")
-## print(sum(en_p == "English")/length(en_p)) 
+cn_p <- predict(m, cn_test$data, type="class")
+print(sum(cn_p == "Chinese")/length(cn_p)) 
 
-## jp_p <- predict(m, jp_test$data, type="class")
-## print(sum(jp_p == "Japanese")/length(jp_p)) 
+en_p <- predict(m, en_test$data, type="class")
+print(sum(en_p == "English")/length(en_p)) 
 
-cn_contrib_p <- predict(m, dt_cn_contrib %>% build_3_gram() %>% mutate(language = factor('Chinese', levels=languages)) , type="class")
-print(sum(cn_contrib_p == "Chinese")/length(cn_contrib_p)) 
+jp_p <- predict(m, jp_test$data, type="class")
+print(sum(jp_p == "Japanese")/length(jp_p)) 
+
+## cn_contrib_p <- predict(m, dt_cn_contrib %>% build_3_gram() , type="class")
+## print(sum(cn_contrib_p == "Chinese")/length(cn_contrib_p)) 
 
 
-gsl_p <- predict(m, gsl_test %>% select(-language))
-sum(gsl_p == "Chinese")/length(gsl_p)
+## gsl_cn_p <- predict(m, dt_gsl_cn %>% build_3_gram() , type="class")
+## print(sum(gsl_cn_p == "English")/length(gsl_cn_p)) 
+
+
+biden_p <- predict(m, dt_biden %>% build_3_gram() , type="class")
+print(sum(biden_p == "English")/length(biden_p)) 
+
+dt_xuzheng_cn <- read_csv("xuzheng_cn.csv", col_names = c("socket_number", "packet_size"), col_types = cols(socket_number = col_integer(), packet_size = col_integer()), comment = "#") %>% filter(socket_number==53)
+
+xuzheng_cn_p <- predict(m, dt_xuzheng_cn %>% build_3_gram() , type="class")
+print(sum(xuzheng_cn_p == "Chinese")/length(xuzheng_cn_p)) 
+
+
+dt_jp_2 <- read_csv("japanese_2.csv", col_names = c("socket_number", "packet_size"), col_types = cols(socket_number = col_integer(), packet_size = col_integer()), comment = "#") %>% filter(socket_number==61)
+
+jp_2_p <- predict(m, dt_jp_2 %>% build_3_gram() , type="class")
+print(sum(jp_2_p == "Japanese")/length(jp_2_p)) 
+
+
+## jp_2_p <- knn(bind_rows(en_train$data,cn_train$data,jp_train$data), dt_jp_2 %>% build_3_gram(), c(en_train$class,cn_train$class,jp_train$class))
+
+## biden_p <- knn(bind_rows(en_train$data,cn_train$data,jp_train$data), dt_biden %>% build_3_gram(), c(en_train$class,cn_train$class,jp_train$class))
